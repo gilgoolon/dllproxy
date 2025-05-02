6ee13664a6dd8091a117539ed059cd16c95efaec
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-05-02 22:36:18 +0300

    bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dllproxy",
-    version="0.2",
+    version="1.0.0",
     description="Generate And Build a Windows DLL Proxy For Any DLL",
     license="MIT",
     long_description=Path("README.md").read_text(),
