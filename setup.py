ea623f98d1f9ee340c837fe3146739a210ec734b
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2025-05-02 22:01:28 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from glob import glob
+from pathlib import Path
+from setuptools import setup, find_packages
+
+setup(
+    name="dllproxy",
+    version="0.1",
+    description="Generate And Build a Windows DLL Proxy For Any DLL",
+    license="MIT",
+    long_description=Path("README.md").read_text(),
+    author="Gil Alpert",
+    author_email="alpertgil@gmail.com",
+    url="https://github.com/gilgoolon/dllproxy",
+    packages=find_packages(),
+    install_requires=[
+        "pefile"
+    ],
+    entry_points={
+        "console_scripts": [
+            "dllproxy-generate = dllproxy:dllproxy_generate"
+        ]
+    },
+    include_package_data=True,
+    package_data={"dllproxy": ["template/**/**"]}
+)
